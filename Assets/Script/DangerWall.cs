3b02d25f7e43c06ddaa28a79325c27cbd6fc75d6
Takashi <gon27.tk08@gmail.com>
2019-02-26 07:47:33 +0900

    初めてのコミット

---
+++ Assets/Script/DangerWall.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DangerWall : MonoBehaviour {
+
+    //オブジェクトと接触したときに呼ばれる
+    void OnCollisionEnter(Collision hit){
+
+        if (hit.gameObject.CompareTag("Player")){ 
+            //現在のシーンの番号を取得
+            int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+            //現在のシーンを再読み込みをする
+            SceneManager.LoadScene(sceneIndex);
+        }
+    }
+}
