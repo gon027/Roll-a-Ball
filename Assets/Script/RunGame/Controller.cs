13510d90a801c78b9489d5b16d061e49b7983754
Takashi <gon27.tk08@gmail.com>
2019-02-26 15:55:02 +0900

    新しいassetを追加

---
+++ Assets/Script/RunGame/Controller.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+    public GameObject Items;
+    
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
---
+++ Assets/Script/RunGame/CreateScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateScript : MonoBehaviour {
+
+    public GameObject green1;
+    public GameObject green2;
+
+    public static int border = 50;
+    
+	void Update () {
+		if(transform.position.z > border){
+            createMap();
+        }
+	}
+
+    void createMap(){
+        if(green1.transform.position.z < border){
+            border += 100;
+            Vector3 temp = new Vector3(0, 0, border);
+            green1.transform.position = temp;
+        }else if(green2.transform.position.z < border){
+            border += 100;
+            Vector3 temp = new Vector3(0, 0, border);
+            green2.transform.position = temp;
+        }
+    }
+}
